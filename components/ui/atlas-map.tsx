8c396595424aec16ca94c74506b792dd06206a83
endernull <ender@end.works>
2026-02-13 10:18:52 +0100

    fix: map rounding

--- components/ui/atlas-map.tsx
+++ components/ui/atlas-map.tsx
@@ -1,39 +1,40 @@
-import darkMap from "@/assets/maps/dark.json";
-import lightMap from "@/assets/maps/light.json";
-import { useTheme } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import darkMap from '@/assets/maps/dark.json';
+import lightMap from '@/assets/maps/light.json';
+import { useTheme } from '@react-navigation/native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export function AtlasMap({ atlas }: { atlas: AtlasDetails }) {
   const theme = useTheme();
 
   return (
-    <MapView
-      style={styles.map}
-      initialRegion={{
-        latitude: Number(atlas?.latitude) ?? 0,
-        longitude: Number(atlas?.longitude) ?? 0,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      }}
-      scrollEnabled={false}
-      customMapStyle={theme.dark ? darkMap : lightMap}
-    >
-      <Marker
-        coordinate={{
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
           latitude: Number(atlas?.latitude) ?? 0,
           longitude: Number(atlas?.longitude) ?? 0,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
         }}
-      />
-    </MapView>
+        scrollEnabled={false}
+        customMapStyle={theme.dark ? darkMap : lightMap}
+      >
+        <Marker
+          coordinate={{
+            latitude: Number(atlas?.latitude) ?? 0,
+            longitude: Number(atlas?.longitude) ?? 0,
+          }}
+        />
+      </MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: { flex: 1, borderRadius: 32, marginHorizontal: 16, overflow: 'hidden' },
   map: {
     flex: 1,
     height: 200,
-    borderRadius: 32,
-    marginHorizontal: 16,
   },
 });
